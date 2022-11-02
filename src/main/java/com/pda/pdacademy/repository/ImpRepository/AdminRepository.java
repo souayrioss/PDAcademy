6d4c89bcbeeecfb748001b56741c5664313742ae
Oussama Souayri <souayri.oussama@gmail.com>
2022-11-02 14:33:25 +0100

    add repository

--- src/main/java/com/pda/pdacademy/repository/ImpRepository/AdminRepository.java
+++ src/main/java/com/pda/pdacademy/repository/ImpRepository/AdminRepository.java
@@ -5,11 +5,21 @@ import com.pda.pdacademy.dao.Idao;
 import com.pda.pdacademy.dao.impDao.AdminDao;
 import com.pda.pdacademy.repository.Irepository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
 public class AdminRepository implements Irepository<Admins> {
     Idao<Admins> adminDao = new AdminDao();
     @Override
     public boolean login(String Email, String Password) {
+        System.out.println("hello");
         Admins admin = new AdminRepository().findByEmail(Email);
+        System.out.println(admin.toString());
         if(admin.getEmail() == null){return false;}
         return  (admin.getPassword().equals(Password)) ? true : false;
     }
@@ -26,7 +36,21 @@ public class AdminRepository implements Irepository<Admins> {
 
     @Override
     public Admins findByEmail(String Email) {
-
-        return null;
+        /*
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PDAcademy");
+        System.out.println("entityManager open "+entityManagerFactory.isOpen());
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.getTransaction().commit();
+        return entityManager.createQuery("SELECT adm FROM Admins adm WHERE adm.email LIKE :email",Admins.class).setParameter("email", Email).getSingleResult();
+         */
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Query<Admins> query = session.createQuery("SELECT adm FROM Admins adm WHERE adm.email LIKE :email",Admins.class);
+        query.setParameter("email", Email);
+        Admins admins = query.getSingleResult();
+        session.close();
+        return admins;
     }
 }
