93702b77ce336873390d25b8189aca56157bf5ee
Oussama Souayri <souayri.oussama@gmail.com>
2022-11-07 17:59:40 +0100

    some update in jsp login

--- src/main/java/com/pda/pdacademy/servlets/LoginServlet.java
+++ src/main/java/com/pda/pdacademy/servlets/LoginServlet.java
@@ -8,8 +8,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
+@WebServlet(name = "login", value = "/login")
+public class LoginServlet extends HttpServlet {
     private String message;
 
     public void init() {
